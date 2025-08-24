9f4f9c9712098bcc044749844319743a305c8276
Guille <gaa1989@hotmail.com>
2025-08-24 13:15:51 +0200

    ajuste cors desde backend

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const uploadsRouter = require('./src/routes/uploads');
-const listRouter = require('./src/routes/list');
-const imagesRouter  = require('./src/routes/images');
-const listFromMongo = require('./src/routes/listFromMongo');
-
+require("dotenv").config();
+const express = require("express");
+const uploadsRouter = require("./src/routes/uploads");
+const listRouter = require("./src/routes/list");
+const imagesRouter = require("./src/routes/images");
+const listFromMongo = require("./src/routes/listFromMongo");
 
 const app = express();
 app.use(express.json());
 
 // CORS (ajusta origins según tu frontend)
-const cors = require('cors');
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://wallokart-gjcc.vercel.app', 'https://wallokart.vercel.app'],
-  methods: ['GET', 'POST', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+const cors = require("cors");
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://wallokart-gjcc.vercel.app",
+      "https://wallokart.vercel.app",
+      "https://wallokart.s3.us-east-1.amazonaws.com",
+    ],
+    methods: ["GET", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.use('/api/uploads', uploadsRouter);
-app.use('/api/list', listRouter);
-app.use('/api/images', imagesRouter);
-app.use('/api/list-mongo', listFromMongo);
-
+app.use("/api/uploads", uploadsRouter);
+app.use("/api/list", listRouter);
+app.use("/api/images", imagesRouter);
+app.use("/api/list-mongo", listFromMongo);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`API escuchando por puerto:${port}`));
